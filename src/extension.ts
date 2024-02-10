5e595d740c9bbd972adb8b7ac9147d2ef628c4f5
Alex Christian <alexchristianqr@gmail.com>
2024-02-10 03:08:09 -0500

    [updated] scripts commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,38 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { showScripts } from './select-script';
+import { scriptActionReloadCommand, scriptActionStopCommand, showScripts } from './select-script';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log("alexxxxxxxxx");
 
-	let arrayDisposables: Array<vscode.Disposable> = []
-
-	let disposable = vscode.commands.registerCommand('run-button-script.run', () => {
-		console.log("Accion del boton para iniciar")
-		showScripts();
+	vscode.window.onDidCloseTerminal(() => {
+		vscode.commands.executeCommand('setContext', 'ext.actionStartCommand', false);
 	});
-	console.log("alexxxxxxxxx", { disposable });
-	arrayDisposables.push(disposable);
-	// context.subscriptions.push(disposable);
 
-	let actionReload = vscode.commands.registerCommand('rerun-button-script.run', () => {
-		console.log("Accion del boton para relanzar")
-	});
-	arrayDisposables.push(actionReload);
-	// context.subscriptions.push(actionReload);
+	const arrayDisposables: Array<vscode.Disposable> = []
 
-	let actionStop = vscode.commands.registerCommand('stop-button-script.run', () => {
-		console.log("Accion del boton para detener")
+	const actionStartCommand = vscode.commands.registerCommand('run-button-script.run', async () => {
+		console.log("Action command start")
+		await showScripts();
 	});
-	arrayDisposables.push(actionStop);
-	// context.subscriptions.push(actionStop);
+
+	const actionReloadCommand = vscode.commands.registerCommand('rerun-button-script.run', async () => {
+		console.log("Action command reload")
+		await scriptActionReloadCommand();
+	});
+
+	const actionStopCommand = vscode.commands.registerCommand('stop-button-script.run', async () => {
+		console.log("Action command stop")
+		await scriptActionStopCommand();
+	});
+
+	arrayDisposables.push(actionStartCommand);
+	arrayDisposables.push(actionReloadCommand);
+	arrayDisposables.push(actionStopCommand);
 
 	arrayDisposables.forEach(item => {
-		console.log({ item });
-
 		context.subscriptions.push(item);
 	})
-
-
 }
 
 export function deactivate() { }
--- src/select-script.ts
+++ src/select-script.ts
@@ -1,8 +1,11 @@
-import { window, workspace, Uri } from 'vscode';
+import * as vscode from 'vscode';
 import { existsSync } from 'fs';
+import { log } from 'console';
+
+var globalWindowTerminal: vscode.Terminal
 
 export async function showScripts() {
-    const wok = workspace.rootPath;
+    const wok = vscode.workspace.rootPath;
 
 
     if (wok) {
@@ -10,23 +13,43 @@ export async function showScripts() {
         const checkPkgManager = existsSync(`${wok}/package-lock.json`);
         const useNpm = checkPkgManager ? true : false;
         const runCommand = useNpm ? 'npm' : 'yarn';
-        const readPakageJson = await workspace.fs.readFile(Uri.file(packageJsonPath));
+        const readPakageJson = await vscode.workspace.fs.readFile(vscode.Uri.file(packageJsonPath));
         const jsonOutput = JSON.parse(readPakageJson.toString());
-        window.showQuickPick(Object.keys(jsonOutput.scripts)).then(async response => {
+        vscode.window.showQuickPick(Object.keys(jsonOutput.scripts)).then(async response => {
             if (response) {
-                const terminal = window.createTerminal({
+                globalWindowTerminal = vscode.window.createTerminal({
                     cwd: wok,
                     hideFromUser: false,
                     name: response,
                 });
-                terminal.show();
+                globalWindowTerminal.show();
                 setTimeout(() => {
-                    terminal.sendText(`${runCommand} run ${response}`);
+                    globalWindowTerminal.sendText(`${runCommand} run ${response}`);
+                    vscode.commands.executeCommand('setContext', 'ext.actionStartCommand', true);
                 }, 1000);
             }
         });
     } else {
-        window.showErrorMessage('Workspace not found');
+        vscode.window.showErrorMessage('Workspace not found');
     }
+
+}
+
+export async function scriptActionReloadCommand() {
+    const wok = vscode.workspace.rootPath;
+    if (!wok) throw new Error("Not found workspace")
+
+    const processId = await globalWindowTerminal.processId
+    console.log("El PID es: ", processId)
+}
+
+export async function scriptActionStopCommand() {
+    const wok = vscode.workspace.rootPath;
+    if (!wok) throw new Error("Not found workspace")
+
+    // console.log(await vscode.commands.getCommands());
+
+    // workbench.action.terminal.new
+    await vscode.commands.executeCommand('setContext', 'workbench.action.terminal.kill', true);
 
 }
